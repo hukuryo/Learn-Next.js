b7d68ed28928968f0aa337787c03fab39d51d99d
hukuryo <ryohei.1998.05.09@gmail.com>
2024-10-23 21:32:21 +0900

    クエリパラメータを表示

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,9 +1,17 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { useSearchParams } from "next/navigation";
+
+const SearchPage = () => {
+  const searchParams = useSearchParams();
+  const query = searchParams.get("query");
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      search page
+    <div>
+      <h1>Search Page</h1>
+      <p>Query: {query}</p>
     </div>
   );
-}
+};
+
+export default SearchPage;
