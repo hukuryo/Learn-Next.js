4688ba23a022098ebdb34b7f92f324ffe027924a
hukuryo <ryohei.1998.05.09@gmail.com>
2024-11-01 19:27:55 +0900

    無駄なソースを削除

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,14 +3,9 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const response = await fetch("https://jsonplaceholder.typicode.com/todos");
-  const data = await response.json();
   return (
     <html>
-      <body>
-        <div>{data.notice}</div>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
