b379e8391cb606efd89d92da70d30a9b9d727980
hukuryo <ryohei.1998.05.09@gmail.com>
2024-11-03 21:47:27 +0900

    PPRをデモ

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,45 @@
-"use cache";
-import { unstable_cacheLife as cacheLife } from "next/cache";
+import React from "react";
+
+type Todo = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
+
+async function fetchTodosWithDelay(): Promise<Todo[]> {
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos", {
+    cache: "no-store",
+  });
+  const data: Todo[] = await response.json();
+  return data;
+}
+
+type TodoListProps = {
+  todos: Todo[];
+};
+
+export const experimental_ppr = true;
+
+function TodoList({ todos }: TodoListProps) {
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <li key={todo.id}>
+          {todo.title} - {todo.completed ? "Completed" : "Not Completed"}
+        </li>
+      ))}
+    </ul>
+  );
+}
 
 export default async function Page() {
-  cacheLife("hours");
-  return <div>Page</div>;
+  const todos = await fetchTodosWithDelay();
+
+  return (
+    <div>
+      <h1>List</h1>
+      <TodoList todos={todos} />
+    </div>
+  );
 }
