150a25e42f0dac08d631c9d08f3a1d974effe552
hukuryo <ryohei.1998.05.09@gmail.com>
2024-11-09 20:51:26 +0900

    optimization package

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   experimental: {
     ppr: "incremental",
-    optimizePackageImports: ["icon-library"],
+    optimizePackageImports: ["react-icons/*"],
   },
 };
 
