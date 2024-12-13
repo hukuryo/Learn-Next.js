5a3cd7c9b2c61f05e97b92b1ee9e6cb026e5203d
hukuryo <ryohei.1998.05.09@gmail.com>
2024-12-13 10:44:48 +0900

    add 401 and 403 page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,9 @@
 import Link from "next/link";
+import { forbidden } from "next/navigation";
 import React, { Suspense } from "react";
 
 export default async function Page() {
+  forbidden();
   return (
     <div>
       <Link href="/dashboard" prefetch>
