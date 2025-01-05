5b24b4766c0a5e5d8f85a8130a8c88c8a3335ac9
hukuryo <ryohei.1998.05.09@gmail.com>
2025-01-06 08:01:40 +0900

    cardの一部をchildrenとして渡す

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,17 @@
+import { Suspense } from "react";
 import { UserCard } from "../../components/UserCard";
+import { getCount } from "./api";
 
 export default async function Page() {
   return (
     <>
-      <UserCard />
+      <UserCard>
+        <Suspense fallback={<div>Loading...</div>}>
+          <p className="font-normal text-gray-700 dark:text-gray-400">
+            投稿数：{getCount()}
+          </p>
+        </Suspense>
+      </UserCard>
     </>
   );
 }
--- components/UserCard.tsx
+++ components/UserCard.tsx
@@ -1,22 +1,20 @@
-import { getCount, getName } from "@/app/(root)/api";
+import { getName } from "@/app/(root)/api";
 import React from "react";
 import { Suspense } from "react";
 
-export const UserCard = () => {
+export const UserCard = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-      <Suspense
-        fallback={<div className="h-5 w-10 animate-pulse bg-zinc-200"></div>}
-      >
-        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-          {getName()}
-        </h5>
-      </Suspense>
-      <Suspense fallback={<div>Loading...</div>}>
-        <p className="font-normal text-gray-700 dark:text-gray-400">
-          投稿数：{getCount()}
-        </p>
-      </Suspense>
+      <div>
+        <Suspense
+          fallback={<div className="h-5 w-10 animate-pulse bg-zinc-200"></div>}
+        >
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            {getName()}
+          </h5>
+        </Suspense>
+      </div>
+      <div>{children}</div>
     </div>
   );
 };
