49269af1ba404bf583be194f97d42dde5c002b70
hukuryo <ryohei.1998.05.09@gmail.com>
2025-01-05 22:22:35 +0900

    APIを追加

--- app/(root)/api.ts
+++ app/(root)/api.ts
@@ -1,10 +1,13 @@
-import { unstable_cacheTag } from "next/cache";
-
 export async function getName() {
-  "use cache";
-  unstable_cacheTag("name");
-
+  await new Promise((resolve) => setTimeout(resolve, 2000));
   const res = await fetch(`http://localhost:3000/api/name`);
   const name = await res.json();
   return name as string;
+}
+
+export async function getCount() {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  const res = await fetch(`http://localhost:3000/api/count`);
+  const count = await res.json();
+  return count as number;
 }
