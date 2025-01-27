3298012c31153c180126985fad2fd164a835db16
hukuryo <ryohei.1998.05.09@gmail.com>
2025-01-27 22:12:16 +0900

    add custom suspense component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,16 +1,21 @@
-import { Suspense } from "react";
+import { CustomSuspense } from "@/util/CustomSuspense";
 import { UserCard } from "../../components/UserCard";
-import { getCount } from "./api";
+import { getCount, getName } from "./api";
 
 export default async function Page() {
   return (
     <>
       <UserCard>
-        <Suspense fallback={<div>Loading...</div>}>
-          <p className="font-normal text-gray-700 dark:text-gray-400">
-            投稿数：{getCount()}
-          </p>
-        </Suspense>
+        名前：
+        <CustomSuspense height={15} width={100}>
+          {getName()}
+        </CustomSuspense>
+      </UserCard>
+      <UserCard>
+        投稿数：
+        <CustomSuspense height={15} width={100}>
+          {getCount()}
+        </CustomSuspense>
       </UserCard>
     </>
   );
--- components/UserCard.tsx
+++ components/UserCard.tsx
@@ -1,20 +1,9 @@
-import { getName } from "@/app/(root)/api";
 import React from "react";
-import { Suspense } from "react";
 
 export const UserCard = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-      <div>
-        <Suspense
-          fallback={<div className="h-5 w-10 animate-pulse bg-zinc-200"></div>}
-        >
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            {getName()}
-          </h5>
-        </Suspense>
-      </div>
-      <div>{children}</div>
+    <div className="block max-w-sm p-6  border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+      {children}
     </div>
   );
 };
