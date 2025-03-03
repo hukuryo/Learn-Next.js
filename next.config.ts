59e65304cd46b7b3ef0ac1285424987840491b71
hukuryo <ryohei.1998.05.09@gmail.com>
2025-03-03 21:34:59 +0900

    add experimental feature

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   experimental: {
     reactCompiler: true,
+    viewTransition: true,
+    nodeMiddleware: true,
   },
 };
 
