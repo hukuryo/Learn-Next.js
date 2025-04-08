9eb6bdc1eba6b44fff8f4ddd1d5feb57f8fcf5c1
hukuryo <ryohei.1998.05.09@gmail.com>
2025-04-09 07:20:08 +0900

    zodを利用

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -2,12 +2,19 @@ import { getUserList, getName } from "./api";
 import { UserCard } from "./_user-card";
 import { CustomSuspense } from "@/src/util/custom-suspense";
 import Link from "next/link";
+import { z } from "zod";
+
+const UserSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+});
 
 async function UserList() {
   const users = await getUserList();
+  const parsedUsers = z.array(UserSchema).parse(users);
   return (
     <ul className="list-inside list-disc border p-4">
-      {users.map((user: { id: string; name: string }) => (
+      {parsedUsers.map((user) => (
         <li key={user.id}>
           <Link href={`/users/${user.id}`}>{user.name}</Link>
         </li>
