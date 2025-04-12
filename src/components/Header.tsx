9ab7e4eff6cc112a3e47fad855257e5a1172820e
hukuryo <ryohei.1998.05.09@gmail.com>
2025-04-12 22:02:16 +0900

    onNavigate

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,18 @@
+"use client";
+
 import Link from "next/link";
-import LoadingIndicator from "./LoadingIndicator";
+// import LoadingIndicator from "./LoadingIndicator";
 
 export default function Header() {
   return (
     <header>
-      <Link href="/dashboard">
-        Dashboard <LoadingIndicator />
+      <Link
+        href="/dashboard"
+        onNavigate={() => {
+          console.log("Navigation...");
+        }}
+      >
+        Dashboard
       </Link>
     </header>
   );
