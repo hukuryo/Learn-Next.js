488a9b991aa4806c196e54560324820b70446bd1
hukuryo <ryohei.1998.05.09@gmail.com>
2025-04-13 09:42:17 +0900

    Turbopack configuration

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,14 @@ const nextConfig: NextConfig = {
     reactCompiler: true,
     viewTransition: true,
   },
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.js",
+      },
+    },
+  },
 };
 
 export default nextConfig;
