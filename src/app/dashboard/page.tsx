a6a156ef50485dfffcb69420ffe8960023953b37
hukuryo <ryohei.1998.05.09@gmail.com>
2025-04-17 06:53:20 +0900

    feature: ViewTransition

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,22 @@
-// import { getName } from "../api";
+import { unstable_ViewTransition as ViewTransition } from "react";
+import Link from "next/link";
 
-export default async function Page() {
+export default function Page() {
   return (
-    <div>
-      <h1 className="text-blue-400">Dashboard Page</h1>
-    </div>
+    <ViewTransition>
+      <div className="container mx-auto px-4 py-8 mt-8 max-w-[1200px]">
+        <ViewTransition name="experimental-label">
+          <b className="text-gray-800 inline-block">{`<ViewTransition>`}</b>
+        </ViewTransition>
+        {/* Go to home page */}
+        <div className="flex justify-between items-center mb-8">
+          <Link href="/" className="text-blue-gray-500 hover:underline">
+            ← Back
+          </Link>
+        </div>
+
+        <h1 className="text-4xl font-bold mb-8">The latest Next.js news</h1>
+      </div>
+    </ViewTransition>
   );
 }
