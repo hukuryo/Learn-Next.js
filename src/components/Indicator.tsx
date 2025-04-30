04d4c947948ebebd608bda4919cce81c6f8e8878
hukuryo <91451759+hukuryo@users.noreply.github.com>
2025-04-30 21:23:42 +0900

    fix: fix style

--- src/components/Indicator.tsx
+++ src/components/Indicator.tsx
@@ -5,9 +5,6 @@ import { useLinkStatus } from "next/link";
 export function Indicator() {
   const { pending } = useLinkStatus();
   return pending ? (
-    <span
-      className="fixed top-0 left-0 z-1 h-[3px]
-                     bg-blue-300 opacity-50 indicator"
-    />
+    <span className="fixed top-0 left-0 z-1 h-[3px] bg-blue-300 opacity-50 indicator" />
   ) : null;
 }
